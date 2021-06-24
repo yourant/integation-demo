e5c419a4eebae4660d227a114100c64ff4ac2261
Kassandra Lim <sandralim298@gmail.com>
2021-06-24 10:27:58 +0800

    Sales orders with multiple items with qty(tested)

--- app/Console/Commands/LazadaTest.php
+++ app/Console/Commands/LazadaTest.php
@@ -41,63 +41,7 @@ class LazadaTest extends Command
      */
     public function handle()
     {
-        $odataClient = (new LazadaLoginController)->login();
-        $lazada = new LazadaController();
-        $orders = $lazada->getOrders();
-        
-        foreach($orders['data']['orders'] as $order){
-            $orderIdArray[] = $order['order_id'];
-            
-            $tempSO[$order['order_id']]['CardCode'] = 'Lazada_C';
-            $tempSO[$order['order_id']]['DocDate'] = $order['created_at'];
-            $tempSO[$order['order_id']]['DocDueDate'] = '2021-06-25'; // Not sure for this one
-            $tempSO[$order['order_id']]['U_Order_ID'] = $order['order_id'];
-            $tempSO[$order['order_id']]['U_Customer_Name'] = $order['customer_first_name'].' '.$order['customer_last_name'];
-        }
-        
-        $orderIds = '['.implode(',',$orderIdArray).']';
-        $orderItems = $lazada->getMultipleOrderItems($orderIds);
-        foreach ($orderItems['data'] as $item) {
-            $orderId = $item['order_id'];
-            $mergedItem[$orderId] = [];
-
-            foreach($item['order_items'] as $orderItem){
-                $sku = $orderItem['sku'];
-                $itemPrice = $orderItem['item_price'];
-                if(array_key_exists($sku, $mergedItem[$orderId])) {
-                    $mergedItem[$orderId][$sku]['Quantity'] += 1;
-                } else {
-                    $mergedItem[$orderId][$sku]['Quantity'] = 1;
-                    $mergedItem[$orderId][$sku]['ItemCode'] = $sku;
-                    $mergedItem[$orderId][$sku]['UnitPrice'] = $itemPrice;
-                }
-            }
-
-            foreach ($mergedItem[$orderId] as $item) {
-                $items[$orderId][] = [
-                    'ItemCode' => $item['ItemCode'], //sample Item Code only
-                    'Quantity' => $item['Quantity'],
-                    "TaxCode" => 'T1',
-                    'UnitPrice' => $item['UnitPrice']
-                ];
-            }
-
-            $tempSO[$orderId]['DocumentLines'] = $items[$orderId];
-        }
-
-        $finalSO = [];
-
-        foreach($tempSO as $key => $value){
-            $finalSO[] = $tempSO[$key];
-        }
-               
-        try {
-            $salesOrder = $odataClient->post('Orders',$finalSO);
-		} catch (\Exception $e) {
-            dd($e->getMessage());
-		}
-        
-
+        //
     }
 
         
