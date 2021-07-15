40d4a5cbf9b4bf809e6c6517991915afbb5913d3
Paul Jao <paul.jao95@gmail.com>
2021-07-16 05:34:28 +0800

    wip ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,6 +52,7 @@
     
     <!-- Scripts -->
     <script type="text/javascript" src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <!-- Page specific scripts-->
     @stack('scripts')
--- resources/views/shopee/dashboard.blade.php
+++ resources/views/shopee/dashboard.blade.php
@@ -3,12 +3,22 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
 
-            <div id="myAlert" class="alert alert-primary" role="alert">
-                A simple primary alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
-              </div>
+        <div aria-live="polite" aria-atomic="true" class="d-flex justify-content-center align-items-center" style="height: 200px;">
+            <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" style="position: absolute; bottom: 2%; right: 1%;">
+                <div class="toast-header">
+                    <strong class="mr-auto">Placeholder Title</strong>
+                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="toast-body">
+                    Placeholder message.
+                </div>
+            </div>
+        </div>
 
+        <div class="col-md-12">           
             <div class="card">
                 <div class="card-header font-weight-bold">Shopee Dashboard</div>
 
@@ -24,7 +34,7 @@
                                 </div>
                                 <div class="card-footer">
                                     <center>
-                                        <a href="#" class="btn btn-primary">
+                                        <a href="#" class="btn btn-primary" id="syncItemBtn">
                                             PROCESS ITEMS
                                         </a>
                                     </center>
@@ -123,11 +133,8 @@
                         </div> --}}
                     </div>
 
-
                     
-                    {{-- {{ __('You are logged in!') }}
-
-                    <form method="POST" action="{{ route('test.index') }}">
+                    {{-- <form method="POST" action="{{ route('test.index') }}">
                         @csrf
                         <input type="submit" value="Get Data">
                     </form> --}}
@@ -140,12 +147,75 @@
 
 @push('scripts')
     <script type="text/javascript">
-        // $(document).ready(function() {
-            console.log($('#myAlert').text());
-            $('#myAlert').on('closed.bs.alert', function () {
-                console.log('success');
-})
-        // });
+    
+        $(document).ready(function() {
+            // console.log($);
+            $('.toast').toast();
+            // $('.toast').toast('show');
+            // console.log($('#myAlert').text());
+            // $('#myAlert').on('closed.bs.alert', function () {
+            //     console.log('success');
+// })
+
+            $('#syncItemBtn').click(function() {
+                console.log('dasdasd');
+                // alert( "Handler for .click() called." );
+            });
+
+            // $('#syncItemBtn').submit(function (e){
+            //     e.preventDefault();
+            //     var url = $(this).attr('action');
+            //     var method = $(this).attr('method');
+            //     var data = $(this).serialize();
+
+            //     $.ajax({
+            //         url: url,
+            //         data: data,
+            //         method: method,
+            //         beforeSend: function() { 
+            //             $(".help-block").remove();
+            //             $( ".form-control" ).removeClass("is-invalid");
+            //             $(".btn-submit").attr("disabled", true);
+            //             $(".btn-submit").html(`<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Sending...`);
+            //         },
+            //         success: function(data){
+            //           if(data.success == false)
+            //           {
+            //             $('.form-check-label').after('<div class="invalid-feedback d-block">Please confirm if all data are correct</div>');
+            //           }
+            //           else
+            //           {
+            //             window.location.reload();
+            //           }
+            //         },
+            //         error: function(response){
+            //             var errors = response.responseJSON;
+            //             $.each(errors.errors, function (index, value) {
+            //                 var id = $("#"+index);
+            //                 id.closest('.form-control')
+            //                 .addClass('is-invalid');
+                            
+            //                 if(id.next('.select2-container').length > 0){
+            //                     id.next('.select2-container').after('<div class="help-block text-danger">'+value+'</div>');
+            //                 }else{
+            //                     id.after('<div class="help-block text-danger">'+value+'</div>');
+            //                 }
+            //             });
+                        
+            //             if($(".is-invalid").length) {
+            //                 $('html, body').animate({
+            //                         scrollTop: ($(".is-invalid").first().offset().top - 95)
+            //                 },500);
+            //             }
+                        
+            //         },
+            //         complete: function() {
+            //             $(".btn-submit").attr("disabled", false);
+            //             $(".btn-submit").html('Submit');
+            //         }
+            //     })
+            // })
+        });
     </script>
 @endpush
 
