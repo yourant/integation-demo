b4c09cadb9fc83b2d747d011024b60f34f898bc7
Paul Jao <paul.jao95@gmail.com>
2021-07-15 17:14:14 +0800

    updated main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -52,5 +49,11 @@
             @yield('content')
         </main>
     </div>
+    
+    <!-- Scripts -->
+    <script type="text/javascript" src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Page specific scripts-->
+    @stack('scripts')
 </body>
 </html>
