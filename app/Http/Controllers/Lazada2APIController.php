ed425ba74b125b4a20a399196d7a5aa6f2c46c76
Kassandra Lim <sandralim298@gmail.com>
2021-12-13 10:10:25 +0800

    Start date ranged 2 days until now

--- app/Http/Controllers/Lazada2APIController.php
+++ app/Http/Controllers/Lazada2APIController.php
@@ -17,7 +17,7 @@ class Lazada2APIController extends Controller
         $lazada2Service = new Lazada2Service();
         $this->client = new LazopClient($lazada2Service->getAppUrl(),$lazada2Service->getAppKey(),$lazada2Service->getAppSecret());
         $this->accessToken = $lazada2Service->getAccessToken();
-        $this->dateStart = date('Y-m-d', strtotime('-2 days')).'T23:59:59+08:00'; // Output: Date start will be yesterday until today.
+        $this->dateStart = date('Y-m-d', strtotime('-3 days')).'T23:59:59+08:00'; // Output: Date start will be yesterday until today.
     }
 
     public function getProducts($skus){
