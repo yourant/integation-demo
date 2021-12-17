46999d75965f9e0a502dc36aafdb4b8f943f7970
Paul Jao <paul.jao95@gmail.com>
2021-12-17 11:20:56 +0800

    Revert "Revert "Merge pull request #71 from webmclink/lazada-sensitve-data""
    This reverts commit 6a2f38c3396568910d18eb8927f3c33485f1a0d8.

--- app/Console/Commands/Lazada2CreditMemo.php
+++ app/Console/Commands/Lazada2CreditMemo.php
@@ -60,7 +60,7 @@ class Lazada2CreditMemo extends Command
         $orderIdArray = [];
 
         while($moreOrders){
-            
+                
             $orders = $lazadaAPI->getReturnedOrders($offset);
 
             if(!empty($orders['data']['orders'])){
@@ -68,6 +68,23 @@ class Lazada2CreditMemo extends Command
                 foreach($orders['data']['orders'] as $order){
                     $orderId = $order['order_id'];
                     array_push($orderIdArray,$orderId);
+                    //Basic Information
+                    $customerName = $order['customer_first_name'].' '.$order['customer_last_name'];
+                    $receiverPhone = $order['address_shipping']['phone'];
+                    //Shipping Address
+                    $sName = $order['address_shipping']['first_name'] .' '. $order['address_shipping']['last_name'];
+                    $sPhone = $order['address_shipping']['phone'];
+                    $sAddress = $order['address_shipping']['address1'];
+                    $sPostCode = $order['address_shipping']['post_code'];
+                    $sCountry = $order['address_shipping']['country'];
+                    $shippingAddress = $sName."\n".$sPhone."\n".$sAddress.', '.$sPostCode.', '.$sCountry;
+                    //Billing Address
+                    $bName = $order['address_billing']['first_name'] .' '. $order['address_billing']['last_name'];
+                    $bPhone = $order['address_billing']['phone'];
+                    $bAddress = $order['address_billing']['address1'];
+                    $bPostCode = $order['address_billing']['post_code'];
+                    $bCountry = $order['address_billing']['country'];
+                    $billingAddress = $bName."\n".$bPhone."\n".$bAddress.', '.$bPostCode.', '.$bCountry;
                     
                     $tempCM[$orderId] = [
                         'CardCode' => $lazadaCustomer->Name,
@@ -75,9 +92,11 @@ class Lazada2CreditMemo extends Command
                         'DocDueDate' => substr($order['created_at'],0,10),
                         'TaxDate' => substr($order['created_at'],0,10),
                         'NumAtCard' => $orderId,
-                        'U_Ecommerce_Type' => 'Lazada_2',
+                        'U_Ecommerce_Type' => 'Lazada_1',
                         'U_Order_ID' => $orderId,
-                        'U_Customer_Name' => $order['customer_first_name'].' '.$order['customer_last_name'],
+                        'U_Basic_Information' => 'Customer Name: '.$customerName."\n".'Receiver Phone: '.$receiverPhone,
+                        'U_Shipping_Address' => $shippingAddress,
+                        'U_Billing_Address' => $billingAddress,
                     ];
                 
                 }
